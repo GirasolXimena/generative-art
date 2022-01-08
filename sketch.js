63b135501640d6fc47a18d0b2501bfff9b306b78
Robert Andrade <srandradejr@gmail.com>
2022-01-08 03:29:30 +0000

    make a cute little flag

--- sketch.js
+++ sketch.js
@@ -1,14 +1,22 @@
 const canvasSketch = require('canvas-sketch');
 
 const settings = {
-  dimensions: [2048, 2048]
+  dimensions: [2048, 2048],
+  // orientation: 'landscape',
+  // units: 'in',
+  // pixelsPerInch: 300
 };
 
-const sketch = () => {
-  return ({ context, width, height }) => {
-    context.fillStyle = 'purple';
-    context.fillRect(0, 0, width, height);
-  };
+const sketch = () => ({ context, width, height }) => {
+  context.fillStyle = 'purple';
+  context.fillRect(0, 0, width, height);
+  context.beginPath()
+  context.arc(width / 2, height / 2, width * 0.2, 0, 2 * Math.PI)
+  context.fillStyle = 'orange'
+  context.lineWidth = 1;
+  context.strokeStyle = 'green'
+  context.fill()
+  context.stroke()
 };
 
 canvasSketch(sketch, settings);
