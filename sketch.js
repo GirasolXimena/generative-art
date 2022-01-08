8d2db574b1dee76005e2eff4bf2f45e211c2baf1
Robert Andrade <srandradejr@gmail.com>
2022-01-08 03:48:03 +0000

    create grid

--- sketch.js
+++ sketch.js
@@ -2,21 +2,42 @@ const canvasSketch = require('canvas-sketch');
 
 const settings = {
   dimensions: [2048, 2048],
-  // orientation: 'landscape',
-  // units: 'in',
-  // pixelsPerInch: 300
+
 };
 
-const sketch = () => ({ context, width, height }) => {
-  context.fillStyle = 'purple';
-  context.fillRect(0, 0, width, height);
-  context.beginPath()
-  context.arc(width / 2, height / 2, width * 0.2, 0, 2 * Math.PI)
-  context.fillStyle = 'orange'
-  context.lineWidth = 1;
-  context.strokeStyle = 'green'
-  context.fill()
-  context.stroke()
+const sketch = () => {
+  const createGrid = (count) => {
+    const points = []
+    for (let x = 0; x < count; x++) {
+      for (let y = 0; y < count; y++) {
+        const u = count <= 1 ? 0.5 : x / (count - 1);
+        const v = count <= 1 ? 0.5 : y / (count - 1);
+        console.log(x, y, u, v, count)
+        points.push([u, v])
+      }
+    }
+    return points
+  }
+
+  const points = createGrid(5)
+  console.log('points', points)
+
+  return ({ context, width, height }) => {
+    context.fillStyle = 'white'
+    context.fillRect(0, 0, width, height)
+
+    points.forEach(([u, v]) => {
+      const x = u * width
+      const y = v * height
+
+      context.beginPath()
+      context.arc(x, y, 100, 0, Math.PI * 2)
+      context.strokeStyle = 'black'
+      context.lineWidth = 40
+      context.stroke()
+
+    })
+  }
 };
 
 canvasSketch(sketch, settings);
